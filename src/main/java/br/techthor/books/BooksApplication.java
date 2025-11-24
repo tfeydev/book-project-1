207aae1465b89e8617152f5ca6ba9244a4e371f2
tfeydev <tfeydeveloper@gmail.com>
2025-11-24 18:23:33 -0300

    feat: initial commit of the Spring Boot Books API - Project 1 (in-memory)

---
+++ src/main/java/br/techthor/books/BooksApplication.java
@@ -0,0 +1,13 @@
+package br.techthor.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(BooksApplication.class, args);
+	}
+
+}
